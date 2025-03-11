3900abbe6c9fc6c5c21076a17b8ed867710e2036
JoseRec <joseandresrc6@gmail.com>
2025-03-10 21:09:36 -0600

    Creacion del repositorio en Github y del proyecto en apache

---
+++ src/test/java/com/tienda_ropa/TiendaRopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_ropa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
