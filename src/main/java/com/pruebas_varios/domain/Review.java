1c11a57147e2c10a3b1d7e7d042ec6a8cd4f333c
kyo <kiory.jp0203@gmail.com>
2025-04-02 11:28:33 -0600

    correcion, por alguna razon se borraron los getters y setters de clase review causando problemas, ya se corrigio y el programa corre perfecto

--- src/main/java/com/pruebas_varios/domain/Review.java
+++ src/main/java/com/pruebas_varios/domain/Review.java
@@ -20,4 +20,50 @@ public class Review {
     private String usuario;
     private String comentario;
     private int calificacion;
+
+    public Long getIdReview() {
+        return idReview;
+    }
+
+    public void setIdReview(Long idReview) {
+        this.idReview = idReview;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+    
+    
+    
+    
+    
 }
